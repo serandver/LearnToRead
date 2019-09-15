96762104505d9f3d5afa7d1022f2b780b38e98ce
serandver <sakhom16@gmail.com>
2019-09-16 00:11:39 +0300

    change initial structure

--- app/src/main/java/com/example/learn/to/read/MainActivity.java
+++ app/src/main/java/com/example/learn/to/read/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.learn.to.read;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected WebView app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init
+        app = (WebView) findViewById(R.id.app);
+
+        //settings
+        app.setInitialScale(1);
+        app.getSettings().setUseWideViewPort(true);
+        app.getSettings().setJavaScriptEnabled(true);
+
+        app.getSettings().setLoadWithOverviewMode(true);
+        app.getSettings().setAllowContentAccess(true);
+        app.getSettings().setDomStorageEnabled(true);
+        app.getSettings().setAllowFileAccessFromFileURLs(true);
+        app.getSettings().setAllowUniversalAccessFromFileURLs(true);
+
+        //load view
+        app.loadUrl("file:///android_asset/app/index.html");
     }
 }
